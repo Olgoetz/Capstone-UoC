0b30f70be74e06610d0110f53e5cbc92b2ad13ab
Oliver Götz <goetzoliver89@gmail.com>
2019-10-02 17:27:34 +0200

    [skip travis] rearranged folder infrastructure

--- Infrastructure/S3-Example/main.tf
+++ Infrastructure/S3-Example/main.tf
@@ -4,7 +4,9 @@
 #########################################################
 provider "aws" {
 
-  region = "eu-central-1"
+  region                  = "eu-central-1"
+  shared_credentials_file = "/Users/olivergoetz/.aws/credentials"
+  profile                 = "sandbox"
 
 }
 
@@ -14,7 +16,15 @@ provider "aws" {
 
 resource "aws_s3_bucket" "mybucket" {
   bucket = "oliver-goetz-capstone-bucket" # must be UNIQUE
-  acl    = "private"                      # restrict public access
+  acl    = "public-read"
+
+  force_destroy = true
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+
+  }
 
   tags = {
     Name   = "OliverGoetz"
